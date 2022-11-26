61e9b33de2004ebc5bd83246bf26546aa523883b
Esme Povirk <esme@codeweavers.com>
2022-11-26 15:30:19 -0600

    Add mixed-mode test for setting current thread apartment state.

---
+++ tools/tests/mixedmode-apt.cs
@@ -0,0 +1,20 @@
+/* Having a native or mixed-mode .exe in our tests-x86 dir causes
+ * problems, and in some cases we want to control what dependencies
+ * are in what path, so just chain to the real exe. */
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+
+class MixedModeExe
+{
+	static int Main()
+	{
+		string exedir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+		var process = new Process();
+		process.StartInfo.FileName = Path.Combine(exedir, "vstests", "mixedapartmentstate.exe");
+		process.Start();
+		process.WaitForExit();
+		return process.ExitCode;
+	}
+}
